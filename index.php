23be615bbeb3e022bd552ef7697695a2558f23ae
rh-mg <rhamon.mg@gmail.com>
2021-07-15 23:21:48 -0300

    Uso e configuração do Cookie nas telas

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+
+if($_COOKIE['usuario']) {
+    $_SESSION['usuario'] = $_COOKIE['usuario'];
+}
+
 if(!$_SESSION['usuario']) {
      header('Location: login.php');
 }
@@ -26,6 +31,9 @@ if(!$_SESSION['usuario']) {
     <body id="page-top">
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+             <!-- Teste Logout by Rhamon-->
+            <span class="usuario">Usuário: <?= $_SESSION['usuario'] ?></span>
+            <a href="logout.php">Sair</a>
             <div class="container">
                 <a class="navbar-brand" href="#page-top">Projeto CRUD</a>
                 <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
@@ -34,8 +42,8 @@ if(!$_SESSION['usuario']) {
                 </button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#portfolio">Portfolio</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#contact">Contact</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="logout.php">Sair</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#contact"><?= $_SESSION['usuario'] ?></a></li>
                     </ul>
                 </div>
             </div>
